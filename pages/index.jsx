60544e8571a55f10b7891f07524f5f3fa9c0277a
Sourav Shaw <98639756+Rav1639@users.noreply.github.com>
2023-02-19 17:31:04 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -35,7 +35,7 @@ export default function Home() {
           content="The official website for Grid Reputation organization"
         />
         <link rel="icon" href="/grLogo.ico"/>
-          <!-- Meta Pixel Code -->
+
 <script>
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -51,7 +51,7 @@ fbq('track', 'PageView');
 <noscript><img height="1" width="1" style="display:none"
 src="https://www.facebook.com/tr?id=727014112331257&ev=PageView&noscript=1"
 /></noscript>
-<!-- End Meta Pixel Code -->
+
       </Head>
       {loading ? (
         <div id="loader">
