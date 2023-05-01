b82d2137025fa25a10a9ca95d80e3ad6255afa10
Sagar Kumar Choudhary <119745688+sagarchy-05@users.noreply.github.com>
2023-05-01 15:14:01 +0530

    Update blogs-home.jsx

--- pages/blogs-home.jsx
+++ pages/blogs-home.jsx
@@ -40,15 +40,19 @@ export default function AboutUs() {
               <div className="mobele">Services</div>
             </Link>
             <Link href="/aboutus">
-              <div className="mobele" onClick={() => {
-                setHeight("0vh");
-                setShown("none");
-              }}>About Us</div>
+              <div
+                className="mobele"
+                onClick={() => {
+                  setHeight("0vh");
+                  setShown("none");
+                }}
+              >
+                About Us
+              </div>
             </Link>
             <Link href="/contactus">
               <div className="mobele">Contact Us</div>
             </Link>
-
           </div>
         </div>
         <nav>
@@ -69,10 +73,15 @@ export default function AboutUs() {
             </Link>
 
             <Link href="/aboutus">
-              <div className="navEle" onClick={() => {
-                setHeight("0vh");
-                setShown("none");
-              }}>About Us</div>
+              <div
+                className="navEle"
+                onClick={() => {
+                  setHeight("0vh");
+                  setShown("none");
+                }}
+              >
+                About Us
+              </div>
             </Link>
             <Link href="/contactus">
               <div className="navEle" id="contactUs">
@@ -97,85 +106,205 @@ export default function AboutUs() {
           <div className={s.card} data-aos="fade-up">
             <div className={s.cardcontent}>
               <h2 className={s.cardtitle}>What is an SEO?</h2>
-              <p className={s.cardbody}>In today's digital world, it is important to have an online presence for your business. But just having a website is not enough. You need to make sure . . .</p>
-              <a href="/blog1" className={s.buttonn}>Learn More</a>
+              <p className={s.cardbody}>
+                In today's digital world, it is important to have an online
+                presence for your business. But just having a website is not
+                enough. You need to make sure . . .
+              </p>
+              <a href="/blog1" className={s.buttonn}>
+                Learn More
+              </a>
             </div>
           </div>
           <div className={s.card} data-aos="fade-up">
             <div className={s.cardcontent}>
               <h2 className={s.cardtitle}>SEO Tips!</h2>
-              <p className={s.cardbody}>SEO is an essential component of digital marketing. It helps to improve the visibility and ranking of your website on search engine results pages (SERPs), . . . </p>
-              <a href="/blog2" className={s.buttonn}>Learn More</a>
+              <p className={s.cardbody}>
+                SEO is an essential component of digital marketing. It helps to
+                improve the visibility and ranking of your website on search
+                engine results pages (SERPs), . . .{" "}
+              </p>
+              <a href="/blog2" className={s.buttonn}>
+                Learn More
+              </a>
             </div>
           </div>
           <div className={s.card} data-aos="fade-up">
             <div className={s.cardcontent}>
               <h2 className={s.cardtitle}>Small Business SEO!</h2>
-              <p className={s.cardbody}>As a small business owner, you might think that investing in Search Engine Optimization (SEO) is an expense you can't afford. However . . .</p>
-              <a href="/blog3" className={s.buttonn}>Learn More</a>
+              <p className={s.cardbody}>
+                As a small business owner, you might think that investing in
+                Search Engine Optimization (SEO) is an expense you can't afford.
+                However . . .
+              </p>
+              <a href="/blog3" className={s.buttonn}>
+                Learn More
+              </a>
             </div>
           </div>
           <div className={s.card} data-aos="fade-up">
             <div className={s.cardcontent}>
-              <h2 className={s.cardtitle}>SEO for Financial<br />Management</h2>
-              <p className={s.cardbody}>With more and more customers relying on search engines to find financial products and services, financial institutions need to ensure . . .</p>
-              <a href="/blog4" className={s.buttonn}>Learn More</a>
+              <h2 className={s.cardtitle}>
+                SEO for Financial
+                <br />
+                Management
+              </h2>
+              <p className={s.cardbody}>
+                With more and more customers relying on search engines to find
+                financial products and services, financial institutions need to
+                ensure . . .
+              </p>
+              <a href="/blog4" className={s.buttonn}>
+                Learn More
+              </a>
             </div>
           </div>
           <div className={s.card} data-aos="fade-up">
             <div className={s.cardcontent}>
               <h2 className={s.cardtitle}>Custom Web Dev!</h2>
-              <p className={s.cardbody}>In today's digital era, having a strong online presence is essential for any business that wants to succeed. A website is a . . .</p>
-              <a href="/blog5" className={s.buttonn}>Learn More</a>
+              <p className={s.cardbody}>
+                In today's digital era, having a strong online presence is
+                essential for any business that wants to succeed. A website is a
+                . . .
+              </p>
+              <a href="/blog5" className={s.buttonn}>
+                Learn More
+              </a>
             </div>
           </div>
           <div className={s.card} data-aos="fade-up">
             <div className={s.cardcontent}>
               <h2 className={s.cardtitle}>php Web Dev!</h2>
-              <p className={s.cardbody}>PHP is a popular server-side scripting language used for web development. With its open-source nature, versatility, and simplicity, PHP has  . . .</p>
-              <a href="/blog6" className={s.buttonn}>Learn More</a>
+              <p className={s.cardbody}>
+                PHP is a popular server-side scripting language used for web
+                development. With its open-source nature, versatility, and
+                simplicity, PHP has . . .
+              </p>
+              <a href="/blog6" className={s.buttonn}>
+                Learn More
+              </a>
             </div>
           </div>
           <div className={s.card} data-aos="fade-up">
             <div className={s.cardcontent}>
-              <h2 className={s.cardtitle}>Social Media <br/>Campaign Strategy!</h2>
-              <p className={s.cardbody}>In today's digital age, social media has become an integral part of our lives, and businesses have recognized the importance of incorporating social media . . .</p>
-              <a href="/blog7" className={s.buttonn}>Learn More</a>
+              <h2 className={s.cardtitle}>
+                Social Media <br />
+                Campaign Strategy!
+              </h2>
+              <p className={s.cardbody}>
+                In today's digital age, social media has become an integral part
+                of our lives, and businesses have recognized the importance of
+                incorporating social media . . .
+              </p>
+              <a href="/blog7" className={s.buttonn}>
+                Learn More
+              </a>
             </div>
           </div>
           <div className={s.card} data-aos="fade-up">
             <div className={s.cardcontent}>
-              <h2 className={s.cardtitle}>Social Media <br/>Marketing Agency!</h2>
-              <p className={s.cardbody}>Looking for ways to increase your brand's online visibility? A social media marketing agency can help you reach your target audience and drive more sales . . .</p>
-              <a href="/blog8" className={s.buttonn}>Learn More</a>
+              <h2 className={s.cardtitle}>
+                Social Media <br />
+                Marketing Agency!
+              </h2>
+              <p className={s.cardbody}>
+                Looking for ways to increase your brand's online visibility? A
+                social media marketing agency can help you reach your target
+                audience and drive more sales . . .
+              </p>
+              <a href="/blog8" className={s.buttonn}>
+                Learn More
+              </a>
             </div>
           </div>
           <div className={s.card} data-aos="fade-up">
             <div className={s.cardcontent}>
-              <h2 className={s.cardtitle}>Social Media <br/>Marketing Plan!</h2>
-              <p className={s.cardbody}>Are you looking to create a social media marketing plan that drives engagement, increases your brand awareness, and boosts sales? In today's . . .</p>
-              <a href="/blog9" className={s.buttonn}>Learn More</a>
+              <h2 className={s.cardtitle}>
+                Social Media <br />
+                Marketing Plan!
+              </h2>
+              <p className={s.cardbody}>
+                Are you looking to create a social media marketing plan that
+                drives engagement, increases your brand awareness, and boosts
+                sales? In today's . . .
+              </p>
+              <a href="/blog9" className={s.buttonn}>
+                Learn More
+              </a>
             </div>
           </div>
           <div className={s.card} data-aos="fade-up">
             <div className={s.cardcontent}>
-              <h2 className={s.cardtitle}>How to increase <br/>Google Reviews?</h2>
-              <p className={s.cardbody}>In today's digital age, online reviews have become an essential component for any business to establish its online reputation . . .</p>
-              <a href="/blog10" className={s.buttonn}>Learn More</a>
+              <h2 className={s.cardtitle}>
+                How to increase <br />
+                Google Reviews?
+              </h2>
+              <p className={s.cardbody}>
+                In today's digital age, online reviews have become an essential
+                component for any business to establish its online reputation .
+                . .
+              </p>
+              <a href="/blog10" className={s.buttonn}>
+                Learn More
+              </a>
             </div>
           </div>
           <div className={s.card} data-aos="fade-up">
             <div className={s.cardcontent}>
-              <h2 className={s.cardtitle}>How to Remove Bad <br/> Reviews from Google?</h2>
-              <p className={s.cardbody}>In today's digital world, businesses of all sizes rely heavily on their online reputation to attract and retain customers. Online . . .</p>
-              <a href="/blog11" className={s.buttonn}>Learn More</a>
+              <h2 className={s.cardtitle}>
+                How to Remove Bad <br /> Reviews from Google?
+              </h2>
+              <p className={s.cardbody}>
+                In today's digital world, businesses of all sizes rely heavily
+                on their online reputation to attract and retain customers.
+                Online . . .
+              </p>
+              <a href="/blog11" className={s.buttonn}>
+                Learn More
+              </a>
             </div>
           </div>
           <div className={s.card} data-aos="fade-up">
             <div className={s.cardcontent}>
               <h2 className={s.cardtitle}>Buy Google Reviews!</h2>
-              <p className={s.cardbody}>If you own a business, you know that having a strong online presence is crucial. One way to boost your online reputation is by having . . .</p>
-              <a href="/blog12" className={s.buttonn}>Learn More</a>
+              <p className={s.cardbody}>
+                If you own a business, you know that having a strong online
+                presence is crucial. One way to boost your online reputation is
+                by having . . .
+              </p>
+              <a href="/blog12" className={s.buttonn}>
+                Learn More
+              </a>
+            </div>
+          </div>
+          <div className={s.card} data-aos="fade-up">
+            <div className={s.cardcontent}>
+              <h2 className={s.cardtitle}>
+                Content Writing <br /> Services!
+              </h2>
+              <p className={s.cardbody}>
+                In today's digital world, content is king. Whether you are a
+                small business owner or a multinational corporation, creating
+                high-quality content . . .
+              </p>
+              <a href="/blog13" className={s.buttonn}>
+                Learn More
+              </a>
+            </div>
+          </div>
+          <div className={s.card} data-aos="fade-up">
+            <div className={s.cardcontent}>
+              <h2 className={s.cardtitle}>
+                SEO Content <br /> Writing Services!
+              </h2>
+              <p className={s.cardbody}>
+                In today's digital age, businesses need to establish a strong
+                online presence to stay competitive. One of the most effective
+                ways to do this is by optimizing . . .
+              </p>
+              <a href="/blog14" className={s.buttonn}>
+                Learn More
+              </a>
             </div>
           </div>
         </div>
@@ -194,8 +323,12 @@ export default function AboutUs() {
                 </Link>
               </div>
               <div id="policyterms">
-                <Link href="/privacypolicy"><div class="policy">Privacy Policy</div></Link>
-                <Link href="/terms"><div class="terms">Terms of Service</div></Link>
+                <Link href="/privacypolicy">
+                  <div class="policy">Privacy Policy</div>
+                </Link>
+                <Link href="/terms">
+                  <div class="terms">Terms of Service</div>
+                </Link>
               </div>
               <div id="socials">
                 <Link href="https://wa.me/message/RVHR3F2ZW66LA1">
