09e329e1a9984f7493fbe29bac0964d1554d0fc4
Sourav Shaw <98639756+Rav1639@users.noreply.github.com>
2024-04-28 04:14:31 +0530

    Update services.jsx

--- pages/services.jsx
+++ pages/services.jsx
@@ -11,6 +11,19 @@ export default function Services() {
   return (
     <div>
       <Head>
+
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11429791782">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11429791782');
+</script>
+
+        
         <title>Services</title>
       </Head>
       <main>
